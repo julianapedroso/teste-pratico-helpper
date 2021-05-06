166058f7ed4d1f2c77f87da9f3275302f3d95f78
julianapedroso <juliana.cristina@hotmail.com.br>
2021-05-05 22:35:13 -0300

    adicionada a lógica de validar campos, adicionar registro e limpar campos

--- scripts.js
+++ scripts.js
@@ -125,33 +125,48 @@
 //         }
 //       }
 
-const registers = [
-  {
-    nome: "Juliana",
-    email: "juliana@gmail.com",
-    registro: "12345678900",
-    cep: "12519602",
-    logradouro: "Rua Senador",
-    numero: "16",
-    bairro: "Chácaras Piaguí",
-    cidade: "São Paulo",
-    estado: "São Paulo",
+const Register = {
+  all: [
+    {
+      nome: "Juliana",
+      email: "juliana@gmail.com",
+      registro: "12345678900",
+      cep: "12519602",
+      logradouro: "Rua Senador",
+      numero: "16",
+      bairro: "Chácaras Piaguí",
+      cidade: "São Paulo",
+      estado: "São Paulo",
+    },
+    {
+      nome: "Lívia",
+      email: "livia@gmail.com",
+      registro: "12345678900",
+      cep: "12519602",
+      logradouro: "Rua Casa 4",
+      numero: "s/n",
+      bairro: "Centro",
+      cidade: "Amaralina",
+      estado: "Goiás",
+    },
+  ],
+
+  add(register) {
+    Register.all.push(register);
+
+    App.reload();
   },
-  {
-    nome: "Lívia",
-    email: "livia@gmail.com",
-    registro: "12345678900",
-    cep: "12519602",
-    logradouro: "Rua Casa 4",
-    numero: "s/n",
-    bairro: "Centro",
-    cidade: "Amaralina",
-    estado: "Goiás",
+
+  remove(index) {
+    Register.all.splice(index, 1);
+
+    App.reload();
   },
-];
+};
 
 const DOM = {
-    registersContainer: document.querySelector('#tabela tbody'),
+  registersContainer: document.querySelector("#tabela tbody"),
+
   addRegister(register, index) {
     const tr = document.createElement("tr");
     tr.innerHTML = DOM.innerHTMLRegister(register);
@@ -171,14 +186,116 @@ const DOM = {
             <td class="cidade">${register.cidade}</td>
             <td class="estado">${register.estado}</td>
             <td>
-                <img onclick="Registers.remove" src="./assets/minus.svg" alt="Imagem de remover" />
+                <img onclick="Registers.remove" src="./assets/remove.svg" alt="Imagem de remover" />
             </td>
       `;
     return html;
   },
+
+  clearRegisters() {
+    DOM.registersContainer.innerHTML = "";
+  },
 };
 
-DOM.addRegister(registers[0]);
+const Form = {
+  nome: document.querySelector("input#nome"),
+  email: document.querySelector("input#email"),
+  registro: document.querySelector("input#registro"),
+  cep: document.querySelector("input#cep"),
+  logradouro: document.querySelector("input#logradouro"),
+  numero: document.querySelect("input#numero"),
+  bairro: document.querySelector("input#bairro"),
+  cidade: document.querySelector("input#cidade"),
+  estado: document.querySelector("input#estado"),
+
+  getValues() {
+    return {
+      nome: Form.nome.value,
+      email: Form.email.value,
+      registro: Form.registro.value,
+      cep: Form.cep.value,
+      logradouro: Form.logradouro.value,
+      numero: Form.numero.value,
+      bairro: Form.bairro.value,
+      cidade: Form.cidade.value,
+      estado: Form.estado.value,
+    };
+  },
+
+  validateFields() {
+    const {
+      nome,
+      email,
+      registro,
+      cep,
+      logradouro,
+      numero,
+      bairro,
+      cidade,
+      estado,
+    } = Form.getValues();
+
+    if (
+      nome.trim() === "" ||
+      email.trim() === "" ||
+      registro.trim() === "" ||
+      cep.trim() === "" ||
+      logradouro.trim() === "" ||
+      numero.trim() === "" ||
+      bairro.trim() === "" ||
+      cidade.trim() === "" ||
+      estado.trim() === ""
+    ) {
+      throw new Error("Necessário o preenchimento de todos os campos");
+    }
+  },
+
+  saveRegister(register) {
+    Register.add(register)
+  },
+
+  clearFields() {
+    Form.nome.value = "",
+    Form.email.value = "",
+    Form.registro.value = "",
+    Form.cep.value = "",
+    Form.logradouro.value = "",
+    Form.numero.value = "",
+    Form.bairro.value = "",
+    Form.cidade.value = "",
+    Form.estado.value = ""
+  },
+
+  submit(event) {
+    event.preventDefault();
+
+    try {
+      Form.validateFields();
+      Form.saveRegister(register);
+      Form.clearFields()
+      
+    } catch (error) {
+      alert(error.message)
+    }
+
+   
+  },
+};
+
+const App = {
+  init() {
+    Register.all.forEach((register) => {
+      DOM.addRegister(register);
+    });
+  },
+
+  reload() {
+    DOM.clearRegisters();
+    App.init();
+  },
+};
+
+App.init();
 
 // let capturandoNome = ""
 
